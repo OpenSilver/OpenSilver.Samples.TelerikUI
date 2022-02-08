41dd7e932f0aa2a33667076330dc803a0dd2fb3a
kernel-coder <kernel.writer@gmail.com>
2022-02-08 18:09:18 +0600

    New tab added for Chart controls and put all charts there.

---
+++ OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/ChartControls/ChartControls.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using Telerik.Windows.Data;
+
+namespace OpenSilver.Samples.TelerikUI
+{
+    public partial class ChartControls : UserControl
+    {
+        public ChartControls()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
