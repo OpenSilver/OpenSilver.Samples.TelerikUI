b1f7d8626184b2a8e0577a591fd8e2822a2cfaf5
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2023-03-17 10:52:46 +1100

    Replace MasonryPanels with WrapPanels

---
+++ OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Other/Internal/WrapPanelWithProgressiveLoading.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace OpenSilver.Samples.TelerikUI
+{
+    public class WrapPanelWithProgressiveLoading : WrapPanel
+    {
+        public WrapPanelWithProgressiveLoading()
+        {
+            ProgressiveRenderingChunkSize = 3;
+        }
+    }
+}
