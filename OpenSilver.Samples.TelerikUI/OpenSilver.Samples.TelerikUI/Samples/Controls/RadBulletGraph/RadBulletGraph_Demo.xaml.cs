b0ed2dccc5ca1e5a6d8c4f0ad596c44ae7b1900c
Pierre Barjon <barjon.pierre@gmail.com>
2025-07-17 09:41:51 +0200

    Add RadBulletGraph demo

---
+++ OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadBulletGraph/RadBulletGraph_Demo.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace OpenSilver.Samples.TelerikUI
+{
+    public partial class RadBulletGraph_Demo : UserControl
+    {
+        public RadBulletGraph_Demo()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonViewSource_Click(object sender, RoutedEventArgs e)
+        {
+            ViewSourceButtonHelper.ViewSource(new List<ViewSourceButtonInfo>()
+            {
+                new ViewSourceButtonInfo()
+                {
+                    TabHeader = "RadBulletGraph_Demo.xaml",
+                    FilePathOnGitHub = "github/OpenSilver/OpenSilver.Samples.TelerikUI/blob/master/OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadBulletGraph/RadBulletGraph_Demo.xaml"
+                },
+                new ViewSourceButtonInfo()
+                {
+                     TabHeader = "RadBulletGraph_Demo.xaml.cs",
+                     FilePathOnGitHub = "github/OpenSilver/OpenSilver.Samples.TelerikUI/blob/master/OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadBulletGraph/RadBulletGraph_Demo.xaml.cs"
+                },
+            });
+        }
+    }
+}
