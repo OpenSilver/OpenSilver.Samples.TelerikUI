1debc6ab0283949ea92151d431c07e50e2a422f2
Pierre Barjon <barjon.pierre@gmail.com>
2025-07-20 23:13:26 +0200

    Add RadRadialMenu demo

---
+++ OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadRadialMenu/RadRadialMenu_Demo.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilver.Samples.TelerikUI
+{
+    public partial class RadRadialMenu_Demo : UserControl
+    {
+        public RadRadialMenu_Demo()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonViewSource_Click(object sender, RoutedEventArgs e)
+        {
+            ViewSourceButtonHelper.ViewSource(new List<ViewSourceButtonInfo>()
+            {
+                new ViewSourceButtonInfo()
+                {
+                    TabHeader = "RadRadialMenu_Demo.xaml",
+                    FilePathOnGitHub = "github/OpenSilver/OpenSilver.Samples.TelerikUI/blob/master/OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadRadialMenu/RadRadialMenu_Demo.xaml"
+                },
+                new ViewSourceButtonInfo()
+                {
+                     TabHeader = "RadRadialMenu_Demo.xaml.cs",
+                     FilePathOnGitHub = "github/OpenSilver/OpenSilver.Samples.TelerikUI/blob/master/OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadRadialMenu/RadRadialMenu_Demo.xaml.cs"
+                },
+            });
+        }
+    }
+}
