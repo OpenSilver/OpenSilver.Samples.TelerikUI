f6c9c03e34f7a3b500a4ea5abb625206b2156294
Pierre Barjon <barjon.pierre@gmail.com>
2025-07-24 00:58:29 +0200

    Add RadTileView demo

---
+++ OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadTileView/RadTileView_Demo.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Telerik.Windows;
+using Telerik.Windows.Controls;
+
+namespace OpenSilver.Samples.TelerikUI
+{
+    public partial class RadTileView_Demo : UserControl
+    {
+        public RadTileView_Demo()
+        {
+            InitializeComponent();
+
+            DataContext = Planet.GetListOfPlanets();
+        }
+
+        private void tileView1_TileStateChanged(object sender, RadRoutedEventArgs e)
+        {
+            RadTileViewItem item = e.OriginalSource as RadTileViewItem;
+            if (item != null)
+            {
+                RadFluidContentControl fluid = item.ChildrenOfType<RadFluidContentControl>().FirstOrDefault();
+                if (fluid != null)
+                {
+                    switch (item.TileState)
+                    {
+                        case TileViewItemState.Maximized:
+                            fluid.State = FluidContentControlState.Large;
+                            break;
+                        case TileViewItemState.Minimized:
+                            fluid.State = FluidContentControlState.Normal;
+                            break;
+                        case TileViewItemState.Restored:
+                            fluid.State = FluidContentControlState.Normal;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void ButtonViewSource_Click(object sender, RoutedEventArgs e)
+        {
+            ViewSourceButtonHelper.ViewSource(new List<ViewSourceButtonInfo>()
+            {
+                new ViewSourceButtonInfo()
+                {
+                    TabHeader = "RadTileView_Demo.xaml",
+                    FilePathOnGitHub = "github/OpenSilver/OpenSilver.Samples.TelerikUI/blob/master/OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadTileView/RadTileView_Demo.xaml"
+                },
+                new ViewSourceButtonInfo()
+                {
+                     TabHeader = "RadTileView_Demo.xaml.cs",
+                     FilePathOnGitHub = "github/OpenSilver/OpenSilver.Samples.TelerikUI/blob/master/OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadTileView/RadTileView_Demo.xaml.cs"
+                },
+            });
+        }
+    }
+}
