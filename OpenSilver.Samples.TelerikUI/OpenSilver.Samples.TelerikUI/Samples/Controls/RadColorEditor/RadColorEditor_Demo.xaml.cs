9633bd0eb89d636a13fdf291739713229799b461
Pierre Barjon <barjon.pierre@gmail.com>
2025-08-11 13:27:45 +0200

    Add RadColorEditor demo

---
+++ OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadColorEditor/RadColorEditor_Demo.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilver.Samples.TelerikUI
+{
+    public partial class RadColorEditor_Demo : UserControl
+    {
+        public RadColorEditor_Demo()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonViewSource_Click(object sender, RoutedEventArgs e)
+        {
+            ViewSourceButtonHelper.ViewSource(new List<ViewSourceButtonInfo>()
+            {
+                new ViewSourceButtonInfo()
+                {
+                    TabHeader = "RadColorEditor_Demo.xaml",
+                    FilePathOnGitHub = "github/OpenSilver/OpenSilver.Samples.TelerikUI/blob/master/OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadColorEditor/RadColorEditor_Demo.xaml"
+                },
+                new ViewSourceButtonInfo()
+                {
+                     TabHeader = "RadColorEditor_Demo.xaml.cs",
+                     FilePathOnGitHub = "github/OpenSilver/OpenSilver.Samples.TelerikUI/blob/master/OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Controls/RadColorEditor/RadColorEditor_Demo.xaml.cs"
+                },
+            });
+        }
+    }
+}
